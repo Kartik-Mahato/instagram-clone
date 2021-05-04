6ea8bde998b560800611c1238fa0553c81e74f02
Kartik Mahato <kartikmahato1996@gmail.com>
2021-05-04 13:10:35 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCJ6mIpD7L7HLI9EjFeQNUiOH7U0c7INcw",
-    authDomain: "instagramclone-44ad2.firebaseapp.com",
-    projectId: "instagramclone-44ad2",
-    storageBucket: "instagramclone-44ad2.appspot.com",
-    messagingSenderId: "361009819989",
-    appId: "1:361009819989:web:0cf8d0f8bff69576a47fdc"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 
